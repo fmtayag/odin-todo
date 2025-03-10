7009b57b4d959aa438ddc5078ffa252d1048f987
Francis Tayag <zyenapz@gmail.com>
2025-03-10 14:36:30 +0800

    Separate data handling to its own module

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import { Todo, Subtask, Topic, Priority } from './model.js';
+
+/* Global names */
+const STORAGE_KEY = "data";
+const GENERAL_NAME = "General";
+
+export function firstSetup() {
+    /* 
+        Sets up the program, assuming the user is using it for the first time
+    */
+    if(localStorage.getItem(STORAGE_KEY) === null) {
+        const generalTopic = new Topic(GENERAL_NAME, "These to-dos are uncategorized");
+        localStorage.setItem(STORAGE_KEY, JSON.stringify({
+                [GENERAL_NAME]: generalTopic,
+            }
+        ));
+        console.log("Set up finished!");
+    }
+    else {
+        console.log("Already set up!");
+    }
+}
+
+export function saveData(data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+export function loadData() {
+    const dryData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    const data = {};
+
+    for(let key in dryData) {
+        const topic = new Topic(dryData[key].title, dryData[key].description);
+
+        for(const dryTodo of dryData[key].toDoCollection) {
+
+            const todo = new Todo(
+                dryTodo.title,
+                dryTodo.description,
+                new Date(dryTodo.dueDate),
+                Priority.hydrate(dryTodo.priority),
+                dryTodo.subtasks.map( subtask => new Subtask(subtask.description, subtask.isDone))
+            )
+            topic.addToCollection(todo);
+        }
+
+        data[topic.title] = topic;
+    }
+
+    return data;
+}
--- src/index.js
+++ src/index.js
@@ -1,58 +1,15 @@
 import { Todo, Subtask, Topic, Priority } from './model.js';
-import { generalTopic, scienceTopic } from './dummy.js';
-
-/* Global names */
-const STORAGE_KEY = "data";
-const GENERAL_NAME = "General";
+import { todo1, todo2, todo3 } from './dummy.js';
+import { firstSetup, saveData, loadData } from './data.js';
 
 firstSetup();
 const myTopics = loadData();
 
-// myTopics["Science"].moveTodo(myTopics["General"], 0);
+// Program flow 
+// myTopics[GENERAL_NAME].addToCollection(todo1);
+// myTopics[GENERAL_NAME].addToCollection(todo2);
+// myTopics[GENERAL_NAME].addToCollection(todo3);
+// myTopics[GENERAL_NAME].removeTodo(0);
+
+// Program end
 saveData(myTopics);
-
-function firstSetup() {
-    /* 
-        Sets up the program, assuming the user is using it for the first time
-    */
-    if(localStorage.getItem(STORAGE_KEY) === null) {
-        const generalTopic = new Topic(GENERAL_NAME, "These to-dos are uncategorized");
-        localStorage.setItem(STORAGE_KEY, JSON.stringify({
-                [GENERAL_NAME]: generalTopic,
-            }
-        ));
-        console.log("Set up finished!");
-    }
-    else {
-        console.log("Already set up!");
-    }
-}
-
-function saveData(data) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-}
-
-function loadData() {
-    const dryData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-    const data = {};
-
-    for(let key in dryData) {
-        const topic = new Topic(dryData[key].title, dryData[key].description);
-
-        for(const dryTodo of dryData[key].toDoCollection) {
-
-            const todo = new Todo(
-                dryTodo.title,
-                dryTodo.description,
-                new Date(dryTodo.dueDate),
-                Priority.hydrate(dryTodo.priority),
-                dryTodo.subtasks.map( subtask => new Subtask(subtask.description, subtask.isDone))
-            )
-            topic.addToCollection(todo);
-        }
-
-        data[topic.title] = topic;
-    }
-
-    return data;
-}
