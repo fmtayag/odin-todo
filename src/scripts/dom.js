9670e4749c606c8ae663cb1bbb56fc965229434f
Francis Tayag <zyenapz@gmail.com>
2025-03-24 17:53:28 +0800

    Neatify button creation

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,10 +1,20 @@
 import { Todo, Topic, Priority } from './model.js';
 import { loadData, saveData } from './data.js';
 
+let selectedTopic = null;
+
+/* Helper functions */
+function createButton(textContent, clickCallback) {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.textContent = textContent;
+    button.addEventListener("click", clickCallback);
+
+    return button;
+}
+
 /* 'myTopics' acts as a data cache */
 const myTopics = loadData();
-
-let selectedTopic = null;
 
 /* Event listeners */
 let boundUpdateTopicListener = null;
@@ -134,29 +144,25 @@ export class DOMHandler {
             for (const todo of myTopics[topic].todos) {
                 const li = document.createElement("li");
                 li.textContent = todo.title;
+                
+                const editButton = createButton("Edit", 
+                    (e) => {
+                        todoModal.show();
+    
+                        todoTitle.value = todo.title;
+                        todoDesc.value = todo.description;
+                        todoDue.value = new Date(todo.dueDate).toISOString().slice(0, 10);
+                        todoPriority.value = todo.priority;
+                        todoDone.checked = todo.isDone;
+                        TodoModal.populateTopicSelect();
+                        todoTopics.value = topic;
+    
+                        boundUpdateTodoListener = updateTodoListener.bind(null, topic, todo.id);
+                        todoForm.addEventListener("submit", boundUpdateTodoListener);
+                    }
+                );
 
-                const editButton = document.createElement("button");
-                editButton.type = "button";
-                editButton.textContent = "Edit";
-                editButton.addEventListener("click", (e) => {
-                    todoModal.show();
-
-                    todoTitle.value = todo.title;
-                    todoDesc.value = todo.description;
-                    todoDue.value = new Date(todo.dueDate).toISOString().slice(0, 10);
-                    todoPriority.value = todo.priority;
-                    todoDone.checked = todo.isDone;
-                    TodoModal.populateTopicSelect();
-                    todoTopics.value = topic;
-
-                    boundUpdateTodoListener = updateTodoListener.bind(null, topic, todo.id);
-                    todoForm.addEventListener("submit", boundUpdateTodoListener);
-                });
-
-                const deleteButton = document.createElement("button");
-                deleteButton.type = "button";
-                deleteButton.textContent = "Delete";
-                deleteButton.addEventListener("click", (e) => {
+                const deleteButton = createButton("Delete", (e) => {
                     const results = myTopics[topic].todos.filter(
                         (t) => t.id !== todo.id
                     );
@@ -188,31 +194,31 @@ export class DOMHandler {
                 this.rebuildDOM();
             });
 
-            const editButton = document.createElement("button");
-            editButton.type = "button";
-            editButton.textContent = "Edit";
-            editButton.addEventListener("click", (e) => {
-                topicModal.show();
+            const editButton = createButton("Edit", 
+                (e) => {
+                    e.stopPropagation();
 
-                topicTitle.value = myTopics[key].title;
-                topicDesc.value = myTopics[key].description;
+                    topicModal.show();
 
-                boundUpdateTopicListener = updateTopicListener.bind(null, key);
-                topicForm.addEventListener("submit", boundUpdateTopicListener);
-                e.stopPropagation();
-            });
+                    topicTitle.value = myTopics[key].title;
+                    topicDesc.value = myTopics[key].description;
 
-            const deleteButton = document.createElement("button");
-            deleteButton.type = "button";
-            deleteButton.textContent = "Delete";
-            deleteButton.addEventListener("click", (e) => {
-                e.stopPropagation();
-                delete myTopics[key]
-                saveData(myTopics);
+                    boundUpdateTopicListener = updateTopicListener.bind(null, key);
+                    topicForm.addEventListener("submit", boundUpdateTopicListener);
+                }
+            );
 
-                selectedTopic = null;
-                this.rebuildDOM();
-            });
+            const deleteButton = createButton("Delete", 
+                (e) => {
+                    e.stopPropagation();
+
+                    delete myTopics[key]
+                    saveData(myTopics);
+    
+                    selectedTopic = null;
+                    this.rebuildDOM();
+                }
+            );
 
             item.appendChild(editButton);
             item.appendChild(deleteButton);
