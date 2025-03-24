7f801c7b53329509955c9147edd1ecc6a93e0713
Francis Tayag <zyenapz@gmail.com>
2025-03-24 17:27:13 +0800

    Make the topics clickable for navigation

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -4,11 +4,13 @@ import { loadData, saveData } from './data.js';
 /* 'myTopics' acts as a data cache */
 const myTopics = loadData();
 
+let selectedTopic = null;
+
 /* Event listeners */
 let boundUpdateTopicListener = null;
 let boundUpdateTodoListener = null;
 
-const createTopicListener = event => { 
+const createTopicListener = event => {
     event.preventDefault();
     createNewTopic();
     topicForm.removeEventListener("submit", createTopicListener);
@@ -36,7 +38,7 @@ const updateTodoListener = (key, id, event) => {
 function createNewTopic() {
     console.log("Hello");
     /* TODO: Add client-side validation later */
-    
+
     const title = topicTitle.value;
     const description = topicDesc.value;
 
@@ -52,11 +54,11 @@ function updateTopic(key) {
     /* TODO: Add client-side validation later */
     const newTitle = topicTitle.value;
     const newDescription = topicDesc.value;
-    
+
     myTopics[key].title = newTitle;
     myTopics[key].description = newDescription;
-    
-    if(key !== newTitle) {
+
+    if (key !== newTitle) {
         myTopics[newTitle] = myTopics[key];
         delete myTopics[key];
     }
@@ -73,9 +75,9 @@ function createNewTodo() {
     const title = todoTitle.value;
     const description = todoDesc.value;
     const due = new Date(todoDue.value);
-    const priority = todoPriority.value; 
+    const priority = todoPriority.value;
     const isDone = todoDone.checked;
-    const topic = todoTopics.value; 
+    const topic = todoTopics.value;
 
     const todo = new Todo(id, title, description, due, priority, isDone);
     myTopics[topic].addTodo(todo);
@@ -92,15 +94,15 @@ function updateTodo(oldTopic, id) {
     const title = todoTitle.value;
     const description = todoDesc.value;
     const due = new Date(todoDue.value);
-    const priority = todoPriority.value; 
+    const priority = todoPriority.value;
     const isDone = todoDone.checked;
-    const topic = todoTopics.value; 
+    const topic = todoTopics.value;
 
-    const changedTopic = topic !== oldTopic; 
-    
+    const changedTopic = topic !== oldTopic;
+
     const matchID = (todo) => todo.id === id;
     const index = myTopics[oldTopic].todos.findIndex(matchID);
-    
+
     if (changedTopic) {
         const todo = new Todo(id, title, description, due, priority, isDone);
         myTopics[oldTopic].todos.splice(index, 1);
@@ -120,52 +122,56 @@ function updateTodo(oldTopic, id) {
 }
 
 export class DOMHandler {
-    static rebuildDOM(){
+    static rebuildDOM() {
         this.#rebuildTopicList();
-        this.#rebuildTodoList("Home");
+        this.#rebuildTodoList(selectedTopic);
     }
 
     static #rebuildTodoList(topic) {
         todos.innerHTML = ``;
-        for(const todo of myTopics[topic].todos) {
-            const li = document.createElement("li");
-            li.textContent = todo.title;
+        if (topic in myTopics) {
+            currentTopic.textContent = topic;
+            for (const todo of myTopics[topic].todos) {
+                const li = document.createElement("li");
+                li.textContent = todo.title;
 
-            const editButton = document.createElement("button");
-            editButton.type = "button";
-            editButton.textContent = "Edit";
-            editButton.addEventListener("click", (e) => {
-                todoModal.show();
+                const editButton = document.createElement("button");
+                editButton.type = "button";
+                editButton.textContent = "Edit";
+                editButton.addEventListener("click", (e) => {
+                    todoModal.show();
 
-                todoTitle.value = todo.title;
-                todoDesc.value = todo.description;
-                todoDue.value = new Date(todo.dueDate).toISOString().slice(0, 10);
-                todoPriority.value = todo.priority;
-                todoDone.checked = todo.isDone;
-                TodoModal.populateTopicSelect();
-                todoTopics.value = topic;
+                    todoTitle.value = todo.title;
+                    todoDesc.value = todo.description;
+                    todoDue.value = new Date(todo.dueDate).toISOString().slice(0, 10);
+                    todoPriority.value = todo.priority;
+                    todoDone.checked = todo.isDone;
+                    TodoModal.populateTopicSelect();
+                    todoTopics.value = topic;
 
-                boundUpdateTodoListener = updateTodoListener.bind(null, topic, todo.id);
-                todoForm.addEventListener("submit", boundUpdateTodoListener);
-            });
-            
-            const deleteButton = document.createElement("button");
-            deleteButton.type = "button";
-            deleteButton.textContent = "Delete";
-            deleteButton.addEventListener("click", (e) => {
-                const results = myTopics[topic].todos.filter(
-                    (t) => t.id !== todo.id
-                );
+                    boundUpdateTodoListener = updateTodoListener.bind(null, topic, todo.id);
+                    todoForm.addEventListener("submit", boundUpdateTodoListener);
+                });
 
-                myTopics[topic].todos = results;
-                saveData(myTopics);
-                this.rebuildDOM();
-            });
+                const deleteButton = document.createElement("button");
+                deleteButton.type = "button";
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", (e) => {
+                    const results = myTopics[topic].todos.filter(
+                        (t) => t.id !== todo.id
+                    );
 
-            li.appendChild(editButton);
-            li.appendChild(deleteButton);
-            todos.appendChild(li);
+                    myTopics[topic].todos = results;
+                    saveData(myTopics);
+                    this.rebuildDOM();
+                });
+
+                li.appendChild(editButton);
+                li.appendChild(deleteButton);
+                todos.appendChild(li);
+            }
         }
+
     }
 
     static #rebuildTopicList() {
@@ -175,6 +181,12 @@ export class DOMHandler {
         for (const key in myTopics) {
             const item = document.createElement("li");
             item.textContent = key;
+            item.dataset.key = key;
+
+            item.addEventListener("click", (e) => {
+                selectedTopic = item.dataset.key;
+                this.rebuildDOM();
+            });
 
             const editButton = document.createElement("button");
             editButton.type = "button";
@@ -187,14 +199,18 @@ export class DOMHandler {
 
                 boundUpdateTopicListener = updateTopicListener.bind(null, key);
                 topicForm.addEventListener("submit", boundUpdateTopicListener);
+                e.stopPropagation();
             });
 
             const deleteButton = document.createElement("button");
             deleteButton.type = "button";
             deleteButton.textContent = "Delete";
             deleteButton.addEventListener("click", (e) => {
+                e.stopPropagation();
                 delete myTopics[key]
                 saveData(myTopics);
+
+                selectedTopic = null;
                 this.rebuildDOM();
             });
 
@@ -227,7 +243,7 @@ export class TodoModal {
 
     static populateTopicSelect() {
         todoTopics.innerHTML = ``;
-        for(const topic in myTopics) {
+        for (const topic in myTopics) {
             const option = document.createElement("option");
             option.value = topic;
             option.textContent = topic;
@@ -245,7 +261,7 @@ export class TopicModal {
             topicModal.show();
             topicTitle.value = '';
             topicDesc.value = '';
-            
+
             topicForm.addEventListener("submit", createTopicListener);
         });
 
