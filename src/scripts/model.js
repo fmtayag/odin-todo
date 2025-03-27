863c7a1c3d81b5117a6547c1fb23d93e37844b9b
Francis Tayag <zyenapz@gmail.com>
2025-03-27 09:13:43 +0800

    Simplify data models

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -15,34 +15,4 @@ export class Topic {
         this.description = description;
         this.todos = todos;
     }
-
-    addTodo(todo) {
-        this.todos.push(todo);
-    }
-    
-    removeTodo(id) {
-        // TODO
-    }
 }
-
-export class Priority {
-    static #_NONE = 0;
-    static #_LOW = 1; 
-    static #_MED = 2;
-    static #_HIGH = 3;
-
-    static get None() {
-        return this.#_NONE;
-    }
-    static get Low() {
-        return this.#_LOW;
-    }
-    static get Medium() {
-        return this.#_MED;
-    }
-    static get High() {
-        return this.#_HIGH;
-    }
-}
-
-
