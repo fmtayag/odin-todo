e734294822ee80b8d2288996483a7dc69b87ae52
Francis Tayag <zyenapz@gmail.com>
2025-03-27 12:24:43 +0800

    Add click event for topics to display todos

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -2,6 +2,13 @@ import { loadData } from "./data";
 import { deleteTopic, openTopicModalForEdit } from "./topic";
 import { createButton } from "./utils";
 
+const listTodos = (key, topic) => {
+    const header = document.querySelector("#currentTopic");
+    const openTodoBtn = document.querySelector("#openTodo");
+    header.textContent = topic.title;
+    openTodoBtn.classList.remove("hidden");
+}
+
 export const listTopics = () => {
     const list = document.querySelector("#topics");
     const data = loadData();
@@ -10,9 +17,16 @@ export const listTopics = () => {
         const topic = data[key];
         const listItem = document.createElement("li");
         listItem.textContent = topic.title;
-
-        const editButton = createButton("Edit", (e) => openTopicModalForEdit(e, key, topic.title, topic.description));
-        const deleteButton = createButton("Delete", (e) => deleteTopic(e, key));
+        listItem.addEventListener("click", () => listTodos(key, topic) );
+        
+        const editButton = createButton("Edit", (e) => {
+            e.stopPropagation();
+            openTopicModalForEdit(e, key, topic.title, topic.description)
+        });
+        const deleteButton = createButton("Delete", (e) => {
+            e.stopPropagation();
+            deleteTopic(e, key)
+        });
 
         listItem.appendChild(editButton);
         listItem.appendChild(deleteButton);
